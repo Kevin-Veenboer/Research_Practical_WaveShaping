dffc8aa23af13d86926a6d50cdcb8a9385f5a265
Kevin-Veenboer <135600479+Kevin-Veenboer@users.noreply.github.com>
2024-01-23 12:27:04 +0100

    Update TiffDisplayer.py

--- TiffDisplayer.py
+++ TiffDisplayer.py
@@ -28,13 +28,14 @@ def TiffShow(path, multiples=True, save=False, cover=None):
                 image_color = image
 
             coords = grid.pop()
-            # cv2.circle(image_color, tuple(coords), 4, (255, 0, 0), -1)
+            cv2.circle(image_color, tuple(coords), 4, (255, 0, 0), -1)
 
-            # cv2.circle(image_color, (614, 610), 550, (255, 0, 0), 3)
+            cv2.circle(image_color, (614, 610), 550, (255, 0, 0), 3)
 
             # Show image
             plt.imshow(image_color, cmap="gray" if image.ndim == 2 else None)
             plt.show()
 
 
-TiffShow(path_2)
+TiffShow(path)
+# TiffShow(path_2)
